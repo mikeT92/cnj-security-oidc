dc521b7f1c3462ec907748e2e284e10eacf05368
mikeT92 <mike@tschutschu.de>
2019-03-29 00:09:58 +0100

    completed Spring version of security showcase

---
+++ cnj-security-oidc-backend-micro/src/main/java/edu/hm/cs/fwp/cloud/cloudtrain/core/boundary/HelloWorld.java
@@ -0,0 +1,17 @@
+package edu.hm.cs.fwp.cloud.cloudtrain.core.boundary;
+
+import edu.hm.cs.fwp.cloud.cloudtrain.core.entity.Message;
+
+import javax.ejb.Stateless;
+import java.util.UUID;
+
+/**
+ * Simple {@code Boundary} that returns welcome messages.
+ */
+@Stateless
+public class HelloWorld {
+
+    public Message getHelloMessage() {
+        return new Message(UUID.randomUUID(), "cloudtrain", "Welcome to Cloud Native Java with MicroProfile!");
+    }
+}
